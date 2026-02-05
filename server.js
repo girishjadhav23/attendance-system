9fb24ed4bdaffe07c81bf8860ce4ab4a3e87fc05
girish_23 <girishjadhav1414@gmail.com>
2026-02-05 12:44:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.static("public"));
 
 // HOME ROUTE (THIS FIXES THE ERROR)
 app.get("/", (req, res) => {
